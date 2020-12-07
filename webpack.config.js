32be83c406c5dd756b4ecbb0bbc2a47da7e39167
YeJun <yj_mb@hotmail.com>
2020-12-07 09:57:46 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -13,8 +13,8 @@ var postcssVars = require('postcss-simple-vars');
 var postcssImport = require('postcss-import');
 
 //const STATIC_PATH = process.env.STATIC_PATH || '/static';
-const STATIC_PATH = 'http://localhost:32265/static/gui/static';
-//const STATIC_PATH = 'https://cdn.mozhua.org/static/gui/static';
+//const STATIC_PATH = 'http://localhost:32265/static/gui/static';
+const STATIC_PATH = 'https://cdn.mozhua.org/static/gui/static';
 
 const base = {
     mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
