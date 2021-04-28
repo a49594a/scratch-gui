1966d70f70b9d64d325e606fff51e28b7d7a7cf6
YeJun <yj_mb@hotmail.com>
2021-04-28 11:58:33 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -13,8 +13,8 @@ var postcssVars = require('postcss-simple-vars');
 var postcssImport = require('postcss-import');
 
 //const STATIC_PATH = process.env.STATIC_PATH || '/static';
-const STATIC_PATH = 'http://localhost:32265/static/gui/static';
-//const STATIC_PATH = 'https://cdn.mozhua.org/static/gui/static';
+//const STATIC_PATH = 'http://localhost:32265/static/gui/static';
+const STATIC_PATH = 'https://cdn.gitblock.cn/static/gui/static';
 
 const base = {
     mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
@@ -120,8 +120,8 @@ module.exports = [
             'react': 'React',
             'react-dom': 'ReactDOM',
             'scratch-vm': 'VirtualMachine',
-            //'scratch-paint': 'PaintEditor',
-            //'scratch-blocks': 'ScratchBlocks',
+            'scratch-paint': 'PaintEditor',
+            'scratch-blocks': 'ScratchBlocks',
             //React: 'react',
             //ReactDOM: 'react-dom'
         },
